b50de1fce1301a02308a48809e5539d7315d0962
zxjk78 <zxjk78@gmail.com>
2022-09-13 16:31:03 +0900

    Feat: Navbar WIP

---
+++ frontend/src/redux/store.ts
@@ -0,0 +1,28 @@
+import { combineReducers, configureStore } from '@reduxjs/toolkit';
+
+// redux persist
+import { persistStore, persistReducer } from 'redux-persist';
+
+import storage from 'redux-persist/lib/storage';
+
+// reducers
+import loginReducer from './slice/login';
+
+const persistConfig = {
+  key: 'root',
+  storage,
+  version: 3,
+  blacklist: [],
+};
+const reducer = combineReducers({
+  // login
+  // login: loginReducer,
+});
+
+const persistedReducer = persistReducer(persistConfig, reducer);
+
+const store = configureStore({
+  reducer: persistedReducer,
+});
+
+export default store;
