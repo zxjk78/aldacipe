371744ecd8d662ed2b7031f98940690625dc759c
jy-lee0626 <lklll369@gmail.com>
2022-10-05 08:34:44 +0900

    carousel_data

--- frontend/src/components/mainpage/CarouselSimilar.tsx
+++ frontend/src/components/mainpage/CarouselSimilar.tsx
@@ -1,4 +1,4 @@
-import React, { useState, FormEvent, ChangeEvent } from 'react';
+import React, { useState, FormEvent, ChangeEvent, useEffect } from 'react';
 import Card from './CarouselCard';
 import Slider from 'react-slick';
 import 'slick-carousel/slick/slick.css';
@@ -7,30 +7,45 @@ import NextArrow from './NextArrow';
 import PrevArrow from './PrevArrow';
 import { refrigeratorRecipe } from '../../api/main'
 // css
-import classes from './CarouselRefrigerator.module.scss';
+import classes from './CarouselSimilar.module.scss';
 import { CardRecipe } from './interface';
 import { recipe } from '../refrigerator/interface';
+import selectImg from '../../assets/select.jpg';
 
-const CarouselSimilar= (props: {searchdata:never[]}) => {
+const CarouselSimilar= (props: {
+  searchData:recipe[];
+  getSearchData: () => recipe[];
+}) => {
+  const [searchResult, setSearchResult] = useState<recipe[]>([])
   const settings = {
-    dots: true,
+    dots: false,
     infinite: true,
     speed: 500,
-    slidesToShow: 5,
-    slidesToScroll: 5,
+    slidesToShow: 3,
+    slidesToScroll: 1,
     nextArrow: <NextArrow />,
     prevArrow: <PrevArrow />,
     // dotsClass: "custom-dots"
   };
 
-  const cardList = props.searchdata;
+  useEffect(() => {
+    const value = props.getSearchData()
+    setSearchResult(value)
+    console.log(searchResult, 'check carousel---')
+  }, [searchResult])
+
+  const cardList = searchResult;
 
   return (
     <>
       <section className={classes.carousel}>
+        {cardList.length === 0 ? 
+          <img src={selectImg} alt="재료를 선택해 주세요" />
+          :<h2>있음</h2>
+        }
         <Slider {...settings}>
-          {cardList.map((card:CardRecipe)=>(<Card card={card} key={card.id}/>))}
-        </Slider>
+          {cardList.map((card:recipe)=>(<Card card={card} key={card.id}/>))}
+        </Slider> 
       </section>
     </>
   );
